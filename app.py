c35d85efd3df0dfa67db20612ec5122e567548ca
soham shrikant manjrekar <91745711+sohammanjrekar@users.noreply.github.com>
2023-10-20 06:19:27 +0530

    upload

--- app.py
+++ app.py
@@ -1,80 +1,49 @@
 import streamlit as st
+import plotly.express as px
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+import matplotlib.pyplot as plt
 import pandas as pd
 from streamlit_option_menu import option_menu
+import pickle
 from PIL import Image
-
-# Set the title and description
-st.title("Spam Detection Project Showcase")
-st.write("Explore different spam detection project ideas using Streamlit.")
-
-# Sidebar navigation
+import numpy as np
+import plotly.figure_factory as ff
+import streamlit as st
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sms import  sms_main
+from email_main import email_main
+from twitter import twitter_main
+from jobs import main as job_main
+from voicemessage import main as audio_main
+from news import  news_main
+# sidebar
 with st.sidebar:
-    selected = option_menu('Spam detection', [
-    "Multilingual Spam Detection",
-    "Spam Detection in Code Repositories",
-    "Image Spam Detection",
-    "Temporal Analysis of Spam Patterns",
-    "Spam Detection in Voice Messages",
-    "Spam Detection in Social Media Comments",
-    "Enhanced Email Spam Detection",
-    "Spam Detection in Video Comments",
-    "Chatbot-based Spam Detection",
-    "Deep Learning for Spam Detection",
-    "Spam Detection for Medical Texts",
-    "Spam Detection in Fake News",
-    "Spam Detection in IoT Data Streams",
-    "User-Specific Spam Filtering",
-    "Adversarial Spam Detection"
-],
-        icons=['activity', 'heart', 'person','list-task'],
+    selected = option_menu('Spam Detection', [
+        'SMS',
+        'EMAIL',
+        'Voice Messages',
+        'Twitter Comments',
+        'News',
+        'Jobs',
+    ],
+        icons=['','activity', 'heart', 'person','person','person'],
         default_index=0)
 
 
 
-if option_menu == "Multilingual Spam Detection":
-    # Insert the code template for Multilingual Spam Detection here
-    pass
-elif option_menu == "Spam Detection in Code Repositories":
-    # Insert the code template for Spam Detection in Code Repositories here
-    pass
-elif option_menu == "Image Spam Detection":
-    # Insert the code template for Image Spam Detection here
-    pass
-elif option_menu == "Temporal Analysis of Spam Patterns":
-    # Insert the code template for Temporal Analysis of Spam Patterns here
-    pass
-elif option_menu == "Spam Detection in Voice Messages":
-    # Insert the code template for Spam Detection in Voice Messages here
-    pass
-elif option_menu == "Spam Detection in Social Media Comments":
-    # Insert the code template for Spam Detection in Social Media Comments here
-    pass
-elif option_menu == "Enhanced Email Spam Detection":
-    # Insert the code template for Enhanced Email Spam Detection here
-    pass
-elif option_menu == "Spam Detection in Video Comments":
-    # Insert the code template for Spam Detection in Video Comments here
-    pass
-elif option_menu == "Chatbot-based Spam Detection":
-    # Insert the code template for Chatbot-based Spam Detection here
-    pass
-elif option_menu == "Deep Learning for Spam Detection":
-    # Insert the code template for Deep Learning for Spam Detection here
-    pass
-elif option_menu == "Spam Detection for Medical Texts":
-    # Insert the code template for Spam Detection for Medical Texts here
-    pass
-elif option_menu == "Spam Detection in Fake News":
-    # Insert the code template for Spam Detection in Fake News here
-    pass
-elif option_menu == "Spam Detection in IoT Data Streams":
-    # Insert the code template for Spam Detection in IoT Data Streams here
-    pass
-elif option_menu == "User-Specific Spam Filtering":
-    # Insert the code template for User-Specific Spam Filtering here
-    pass
-elif option_menu == "Adversarial Spam Detection":
-    # Insert the code template for Adversarial Spam Detection here
-    pass
-else:
-    st.write("Select a project from the sidebar to get started.")
+# Diabetes prediction page
+if selected == 'SMS':
+    sms_main()  
+if selected == 'EMAIL':
+    email_main()  
+if selected == 'Jobs':
+    job_main() 
+if selected == 'Voice Messages':
+    audio_main() 
+if selected == 'Twitter Comments':
+    twitter_main() 
+if selected == 'News':
+    news_main()    
+
